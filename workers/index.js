3f1677c77007767db3e63a31c42f52bca802250a
Jamar Powell <twoyearsyoung@gmail.com>
2019-08-16 09:02:29 -0700

    update github worker

--- workers/index.js
+++ workers/index.js
@@ -1,7 +1,16 @@
+let fetchgithub = require('./tasks/fetchGithub');
+
+//* Default, Scheduler set to every minute.
+
 let CronJob = require('cron').CronJob;
-new CronJob('* * * * * *', function () {
-  console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
+new CronJob('* * * * * *', fetchgithub()
+  , null, true, 'America/Los_Angeles');
+
+
+//̶ F̶e̶t̶c̶h̶ G̶i̶t̶h̶u̶b̶(̶X̶)̶
+// TODO Fetch Indeed(?)
+// TODO Fetch GlassDoor(?)
+
 
 
 //! Cron Setting for once and hour every day for "12 months 0 1/1 * Jan-dec 0-6"
--- workers/tasks/fetchGithub.js
+++ workers/tasks/fetchGithub.js
@@ -1,27 +1,52 @@
 
 let fetch = require("node-fetch");
+let redis = require("redis");
+
+let client = redis.createClient();
+
+const { promisify } = require('util');
+// const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+
+
 const srcURL = "https://jobs.github.com/positions.json";
 // * Start at page 1.
 
-
-const fetchGithub = async () => {
+async function fetchGithub() {
 
   let resultCount = 1;
   let pageNumber = 0;
 
   const allJobs = [];
+  //* Array for jobs
 
-  while(resultCount != 0) {
-    
-    
-      //* Sub Routine
-      const res = await fetch(`srcURL?page=${pageNumber}`);
-      const jobs = await res.json();
-      // allJobs - Push into array?
-      console.log(jobs.length)
+  while (resultCount != 0) {
+
+
+    //* Sub Routine
+    const res = await fetch(`${srcURL}?page=${pageNumber}`);
+    const jobs = await res.json();
+    await allJobs.push(...jobs);
+
+    resultCount = jobs.length;
+    console.log(resultCount);
+    pageNumber++;
+
   }
+  console.log("Job Done.");
+
+
+  const finished = await setAsync("github", JSON.stringify(allJobs));
+  console.log(allJobs.length);
+  // for(i=0; i< allJobs.length; i++){
+  //   // console.log(allJobs[i].title);
+  // }
+
+  console.log({ finished });
 }
 
-fetchGithub();
+// fetchGithub();
+
 
 module.exports = fetchGithub;
